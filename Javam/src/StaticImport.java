415680a0dfc9815f1f9e6a9d0b0c7cb0aeac3348
ADY <devrimytr@yahoo.com>
2019-06-05 10:55:02 +0100

    Moved

--- Javam/src/StaticImport.java
+++ Javam/src/StaticImport.java
@@ -1,11 +1,13 @@
 import java.util.List;
 import static java.util.Arrays.asList;
-import static java.util.Arrays.*;
+//import static java.util.Arrays.*;
 //import static java.util.Arrays;	 //does not compile,trying to import whole class,
 									//but it needs to import members
 public class StaticImport {
 	public static void main(String[] args) {
 		List<String> list = asList("one", "two");
+		//List<String> mylist = Arrays.asList("one", "two"); //does not compile, Arrays class
+															//is not imported "Arrays.asList;" is imported
 		System.out.println(list);
 	}
 }
