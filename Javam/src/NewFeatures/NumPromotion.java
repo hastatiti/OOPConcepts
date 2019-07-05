1710c395d9323aa1bfb188c1d9b91d0e7f27e2f2
ADY <devrimytr@yahoo.com>
2019-07-05 12:22:04 +0100

    Add method

---
+++ Javam/src/NewFeatures/NumPromotion.java
@@ -0,0 +1,15 @@
+package NewFeatures;
+
+public class NumPromotion {
+	public static void main(String[] args) {
+		double d = 1.2;
+		float f = 1.2f;
+		float f2 = 1.2f;
+		System.out.println(d + f);
+		System.out.println(f+f2);
+		int a = 1;
+		System.out.println("///////");
+		System.out.println(d+a);
+		System.out.println(f+a);
+	}
+}
