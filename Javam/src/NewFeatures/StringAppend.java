4b4a4a13cf40c5c9c89b4a7faae055511dfc3768
ADY <devrimytr@yahoo.com>
2019-07-12 21:32:32 +0100

    Add method

--- Javam/src/NewFeatures/StringAppend.java
+++ Javam/src/NewFeatures/StringAppend.java
@@ -1,10 +1,10 @@
 package NewFeatures;
 
 public class StringAppend {
-public static void main(String[] args) {
-	String s = "animal";
-	String s2 = s.replace("a", "A");
-	System.out.println(s2);
-	System.out.println(s2.toLowerCase());
-}
+	public static void main(String[] args) {
+		String s = "animal";
+		String s2 = s.replace("a", "A");
+		System.out.println(s2);
+		System.out.println(s2.toLowerCase());
+	}
 }
