fe6c3e5205aaee1b092eff74883a03468d806caa
ADY <devrimytr@yahoo.com>
2019-07-13 21:31:45 +0100

    Add method

---
+++ Javam/src/NewFeatures/DateAndTime.java
@@ -0,0 +1,19 @@
+package NewFeatures;
+
+import java.time.*;
+
+public class DateAndTime {
+	private static void performAnimalEnrichment(LocalDate start, LocalDate end) {
+		LocalDate upTo = start;
+		while (upTo.isBefore(end)) { // check if still before end
+			System.out.println("give new toy: " + upTo);
+			upTo = upTo.plusMonths(1); // add a month
+		}
+	}
+
+	public static void main(String[] args) {
+		LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
+		LocalDate end = LocalDate.of(2015, Month.MARCH, 30);
+		performAnimalEnrichment(start, end);
+	}
+}
