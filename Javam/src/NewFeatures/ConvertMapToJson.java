543fef85deac9313589b89fbc7446fafab6432cb
ADY <devrimytr@yahoo.com>
2019-07-29 22:17:51 +0100

    Convert JSON

---
+++ Javam/src/NewFeatures/ConvertMapToJson.java
@@ -0,0 +1,25 @@
+package NewFeatures;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+
+public class ConvertMapToJson {
+	public static void main(String[] args) {
+		Map<String, String> elements = new HashMap<>();
+		elements.put("Key1", "Value1");
+		elements.put("Key2", "Value2");
+		elements.put("Key3", "Value3");
+
+		// JSONObject json = new JSONObject(elements);
+		//
+		// System.out.println(json);
+
+		Gson gson = new Gson();
+		String json = gson.toJson(elements);
+		System.out.println(json);
+	}
+}
