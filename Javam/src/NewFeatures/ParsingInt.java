026e6b7c065ed43a4c152f785577755709f65fe8
ADY <devrimytr@yahoo.com>
2019-08-01 10:22:28 +0100

    Add method

---
+++ Javam/src/NewFeatures/ParsingInt.java
@@ -0,0 +1,13 @@
+package NewFeatures;
+
+public class ParsingInt {
+	
+	public static void main(String[] args) {
+		int a = Integer.parseInt("1");
+		Integer b = Integer.valueOf("12");
+		int c  = Integer.valueOf("23");
+		System.out.println(c);
+		boolean d = Boolean.parseBoolean("true");
+		System.out.println(d);
+	}
+}
