8b1be162d870cf4898ce9984f902686bcbcbbebc
ADY <devrimytr@yahoo.com>
2019-08-26 13:47:08 +0100

    Add method

--- Javam/src/NewFeatures/ConvertMapToJson.java
+++ Javam/src/NewFeatures/ConvertMapToJson.java
@@ -13,11 +13,9 @@ public class ConvertMapToJson {
 		elements.put("Key1", "Value1");
 		elements.put("Key2", "Value2");
 		elements.put("Key3", "Value3");
-
 		// JSONObject json = new JSONObject(elements);
 		//
 		// System.out.println(json);
-
 		Gson gson = new Gson();
 		String json = gson.toJson(elements);
 		System.out.println(json);
