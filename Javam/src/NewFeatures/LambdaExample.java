ad47f5f352ecd9a679516f527f37591082e39992
ADY <devrimytr@yahoo.com>
2019-08-29 13:02:32 +0100

    Add method

---
+++ Javam/src/NewFeatures/LambdaExample.java
@@ -0,0 +1,13 @@
+package NewFeatures;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaExample {
+	public static void main(String[] args) {
+		List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API"); 
+//		for (String n : features) { System.out.println(n); }
+	//	features.forEach( n -> System.out.println(n));
+		features.forEach((String n) -> System.out.println(n));
+	}
+}
